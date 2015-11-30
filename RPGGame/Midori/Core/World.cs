131c21618f7c00433fff90da9b3f4d212126bd2e
b1o <danail.stoqnov@gmail.com>
2015-11-30 22:27:21 +0200

    Fuck Visual Studio Source Control

---
+++ RPGGame/Midori/Core/World.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Midori.GameObjects.Units;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Midori.Interfaces;
+
+namespace Midori.Core
+{
+    public static class World
+    {
+        public static bool isCollidedWithWorldBounds(GraphicsDeviceManager graphics, Unit unit)
+        {
+            if (unit.X > graphics.GraphicsDevice.Viewport.Width)
+            {
+                unit.X = 0;
+                return true;
+            }
+            if (unit.X < 0)
+            {
+                unit.X = graphics.GraphicsDevice.Viewport.Width;
+                return true;
+            }
+            return false;
+        }
+
+        public static bool CollidesWith(ICollidable firstObj, ICollidable secondObj)
+        {
+            return firstObj.BoundingBox.Intersects(secondObj.BoundingBox);
+        }
+
+        
+    }
+}
