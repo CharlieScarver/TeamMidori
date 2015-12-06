de6b52d6f34707eda2f74c245861a09add0ea6a9
CharlieScarver <s.apostolovd@gmail.com>
2015-12-06 02:24:52 +0200

    Dafuq commit?

---
+++ RPGGame/Midori/Interfaces/IAnimatableUnit.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Midori.Interfaces
+{
+    public interface IAnimatableUnit : IAnimatable
+    {
+        void AnimateRunningLeft(GameTime gameTime);
+
+        void AnimateRunningRight(GameTime gameTime);
+
+        void AnimateIdle(GameTime gameTime);
+
+        void AnimateJumpRight(GameTime gameTime);
+
+        void AnimateJumpLeft(GameTime gameTime);
+
+        void AnimateFallRight(GameTime gameTime);
+
+        void AnimateFallLeft(GameTime gameTime);
+    }
+}
---
+++ RPGGame/Midori/Interfaces/IMoveable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Midori.Interfaces
+{
+    public interface IMoveable
+    {
+        float MovementSpeed { get; set; }
+    }
+}
